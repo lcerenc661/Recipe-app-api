8464d94a58cd9b23d9e3a05fd067e13671fbe92b
Luis Ceren <luiscerenc18@gmail.com>
2023-08-08 09:45:11 -0500

    User API's enabled

---
+++ app/user/urls.py
@@ -0,0 +1,13 @@
+""""URL mappings for the user API."""
+
+from django.urls import path
+
+from user import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+]
