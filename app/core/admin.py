39193a4bc0d99e3ac3e22ea1f44a61ac1621a084
Luis Ceren <luiscerenc18@gmail.com>
2023-08-08 21:05:19 -0500

    Tag model created

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core.models import User
+from core.models import User, Recipe, Tag
 
 
 class UserAdmin(BaseUserAdmin):
@@ -50,3 +50,5 @@ class UserAdmin(BaseUserAdmin):
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+admin.site.register(Recipe)
+admin.site.register(Tag)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,11 @@ from core import models
 from decimal import Decimal
 
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """Test for models."""
 
@@ -68,3 +73,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_creating_tag(self):
+        """Test creating a tag is successfuil"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
